9aa3055ef442ea7f0898c1840cc7c4893e9a4685
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-06-12 20:30:10 -0300

    add_token

--- game.py
+++ game.py
@@ -9,16 +9,35 @@ import math
 ROWS = 6
 COLS = 7
 board = []
+winner = False
+turn = 0
+
 
 def create_board():
     for _ in range(ROWS):
-        board.append([0]*COLS)
+        board.append(['⚪'] * COLS)
 
 def print_board():
-    for row in range(ROWS):
+    for row in range(ROWS-1, -1, -1):
         print(" ")
         for col in range(COLS):
             print(board[row][col],end=' ')
 
+def add_token(board, col, token):
+    if board[ROWS -1][col] == '⚪':
+        pass
 create_board()
 print_board()
+
+while winner == False:
+    if turn % 2 == 0:
+        print('\nVez do jogador 1')
+        J1 = int(input('\nSelecione uma coluna (0-6): '))
+        turn += 1
+        add_token(board, J1, 'J1')
+        print_board()
+    else:
+        print('\nVez do jogador 2')
+        J2 = int(input('\nSelecione uma coluna (0-6): '))
+        turn +=1
+        print_board()
